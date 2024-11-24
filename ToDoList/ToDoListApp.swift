f6822e32ba28b9b780f45993d6e5dbf4c03372fd
mehrshad <mehrcodeland@gmail.com>
2024-11-24 20:09:52 +0330

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by mehrshad on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
